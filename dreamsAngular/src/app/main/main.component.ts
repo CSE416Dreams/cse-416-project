7da6ab2feecba722a0f8553e4d7d02c33d14b821
paulhan0527 <paul.han0527@gmail.com>
2022-02-07 00:59:37 -0500

    added random map for testing

--- dreamsAngular/src/app/main/main.component.ts
+++ dreamsAngular/src/app/main/main.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
+import * as geojson from 'geojson';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
-export class MainComponent implements OnInit {
+export class MainComponent implements AfterViewInit {
 
-  constructor() { }
+  private initMap(): void {
+    const map = L.map('map').setView([51, 0],13);
 
-  ngOnInit(): void {
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      minZoom: 3,
+      maxZoom: 18,
+      attribution: '<a href=”http://www.openstreetmap.org/copyright">OpenStreetMap</a> '
+    }).addTo(map);
   }
-
+  constructor() { }
+  
+  ngAfterViewInit(): void {
+    this.initMap();
+  }
 }
