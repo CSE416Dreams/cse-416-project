b7e3ed92941c87b423274726d2a2b560d8b52471
paulhan0527 <paul.han0527@gmail.com>
2022-02-07 21:52:28 -0500

    tried but failed loading the maps

--- dreamsAngular/src/app/main/main.component.ts
+++ dreamsAngular/src/app/main/main.component.ts
@@ -1,6 +1,9 @@
 import { AfterViewInit, Component, OnInit } from '@angular/core';
 import * as L from 'leaflet';
 import * as geojson from 'geojson';
+import * as $ from '../../../node_modules/jquery/dist/jquery.min.js'
+
+import * as worldMap from '../../../../State_GEOJSON/northAmerica.json';
 
 @Component({
   selector: 'app-main',
@@ -8,19 +11,38 @@ import * as geojson from 'geojson';
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements AfterViewInit {
-
+  
   private initMap(): void {
-    const map = L.map('map').setView([51, 0],13);
+    // const map = L.map('map').setView([0, 0], 1);
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      minZoom: 3,
-      maxZoom: 18,
-      attribution: '<a href=”http://www.openstreetmap.org/copyright">OpenStreetMap</a> '
-    }).addTo(map);
+    // L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    //   minZoom: 3,
+    //   maxZoom: 18,
+    //   attribution: '<a href=”http://www.openstreetmap.org/copyright">OpenStreetMap</a> '
+    // }).addTo(map);
+    
+    var path = '../../../../State_GEOJSON/worldMap.json';
+    var myCustomStyle = {
+      stroke: false,
+      fill: true,
+      fillColor: '#fff',
+      fillOpacity: 1
   }
-  constructor() { }
+    $.getJSON(path, function(data) {
+      var map = L.map('map').setView([39.74, -105], 4);
+
+      L.geoJSON(data, {
+        style: myCustomStyle  
+      }).addTo(map);
+    })
+  }
+
+  constructor() { 
+  }
+  
   
   ngAfterViewInit(): void {
     this.initMap();
+    
   }
 }
