a67ec153d1feecfee831e2c996ac462481052eb0
Paul Han <paul.han0527@gmail.com>
2022-05-11 23:53:12 -0400

    box and whisker sample done

--- frontend/src/app/main-content/sidenav-content/district-plan-content/district-plan-content.component.ts
+++ frontend/src/app/main-content/sidenav-content/district-plan-content/district-plan-content.component.ts
@@ -126,17 +126,4 @@ export class DistrictPlanContentComponent implements OnInit {
   onDeactivate(data): void {
     // console.log('Deactivate', JSON.parse(JSON.stringify(data)));
   }
-  public generateDayWiseTimeSeries(baseval, count, yrange) {
-    var i = 0;
-    var series = [];
-    while (i < count) {
-      var y =
-        Math.floor(Math.random() * (yrange.max - yrange.min + 1)) + yrange.min;
-
-      series.push([baseval, y]);
-      baseval += 86400000;
-      i++;
-    }
-    return series;
-  }
 }
